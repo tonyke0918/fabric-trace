9a8657cbed70cc71be20f651e0e0ebad8f3ae7f8
tonyke0918 <48613416+tonyke0918@users.noreply.github.com>
2024-05-17 13:27:46 +0800

    Update index.vue

--- application/web/src/views/uplink/index.vue
+++ application/web/src/views/uplink/index.vue
@@ -1,89 +1,89 @@
 <template>
   <div class="uplink-container">
     <div style="color:#909399;margin-bottom: 30px">
-      当前用户：{{ name }};
+      當前用户：{{ name }};
       用户角色: {{ userType }}
     </div>
     <div>
       <el-form ref="form" :model="tracedata" label-width="80px" size="mini" style="">
-        <el-form-item v-show="userType!='种植户'&userType!='消费者'" label="溯源码:" style="width: 300px" label-width="120px">
+        <el-form-item v-show="userType!='養殖戶'&userType!='消費者'" label="溯源碼:" style="width: 300px" label-width="120px">
           <el-input v-model="tracedata.traceability_code" />
         </el-form-item>
 
-        <div v-show="userType=='种植户'">
-          <el-form-item label="农产品名称:" style="width: 300px" label-width="120px">
+        <div v-show="userType=='養殖戶'">
+          <el-form-item label="產品名稱:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Farmer_input.Fa_fruitName" />
           </el-form-item>
-          <el-form-item label="产地:" style="width: 300px" label-width="120px">
+          <el-form-item label="產地:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Farmer_input.Fa_origin" />
           </el-form-item>
-          <el-form-item label="种植时间:" style="width: 300px" label-width="120px">
+          <el-form-item label="養殖時間:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Farmer_input.Fa_plantTime" />
           </el-form-item>
-          <el-form-item label="采摘时间:" style="width: 300px" label-width="120px">
+          <el-form-item label="屠宰時間:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Farmer_input.Fa_pickingTime" />
           </el-form-item>
-          <el-form-item label="种植户名称:" style="width: 300px" label-width="120px">
+          <el-form-item label="養殖戶名稱:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Farmer_input.Fa_farmerName" />
           </el-form-item>
         </div>
-        <div v-show="userType=='工厂'">
-          <el-form-item label="商品名称:" style="width: 300px" label-width="120px">
+        <div v-show="userType=='加工廠'">
+          <el-form-item label="商品名稱:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Factory_input.Fac_productName" />
           </el-form-item>
-          <el-form-item label="生产批次:" style="width: 300px" label-width="120px">
+          <el-form-item label="生產批次:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Factory_input.Fac_productionbatch" />
           </el-form-item>
-          <el-form-item label="生产时间:" style="width: 300px" label-width="120px">
+          <el-form-item label="生產時間:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Factory_input.Fac_productionTime" />
           </el-form-item>
-          <el-form-item label="工厂名称与厂址:" style="width: 300px" label-width="120px">
+          <el-form-item label="工廠名與廠址:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Factory_input.Fac_factoryName" />
           </el-form-item>
-          <el-form-item label="联系电话:" style="width: 300px" label-width="120px">
+          <el-form-item label="聯絡電話:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Factory_input.Fac_contactNumber" />
           </el-form-item>
         </div>
-        <div v-show="userType=='运输司机'">
+        <div v-show="userType=='物流公司'">
           <el-form-item label="姓名:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Driver_input.Dr_name" />
           </el-form-item>
-          <el-form-item label="年龄:" style="width: 300px" label-width="120px">
+          <el-form-item label="年齡:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Driver_input.Dr_age" />
           </el-form-item>
-          <el-form-item label="联系电话:" style="width: 300px" label-width="120px">
+          <el-form-item label="聯絡電話:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Driver_input.Dr_phone" />
           </el-form-item>
-          <el-form-item label="车牌号:" style="width: 300px" label-width="120px">
+          <el-form-item label="車牌號:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Driver_input.Dr_carNumber" />
           </el-form-item>
-          <el-form-item label="运输工具:" style="width: 300px" label-width="120px">
+          <el-form-item label="運輸工具:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Driver_input.Dr_transport" />
           </el-form-item>
         </div>
         <div v-show="userType=='商店'">
-          <el-form-item label="存入时间:" style="width: 300px" label-width="120px">
+          <el-form-item label="入庫時間:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Shop_input.Sh_storeTime" />
           </el-form-item>
-          <el-form-item label="销售时间:" style="width: 300px" label-width="120px">
+          <el-form-item label="賣出時間:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Shop_input.Sh_sellTime" />
           </el-form-item>
-          <el-form-item label="商店名称:" style="width: 300px" label-width="120px">
+          <el-form-item label="商店名稱:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Shop_input.Sh_shopName" />
           </el-form-item>
-          <el-form-item label="商店位置:" style="width: 300px" label-width="120px">
+          <el-form-item label="商店地址:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Shop_input.Sh_shopAddress" />
           </el-form-item>
-          <el-form-item label="商店电话:" style="width: 300px" label-width="120px">
+          <el-form-item label="商店電話:" style="width: 300px" label-width="120px">
             <el-input v-model="tracedata.Shop_input.Sh_shopPhone" />
           </el-form-item>
         </div>
       </el-form>
       <span slot="footer" style="color: gray;" class="dialog-footer">
-        <el-button v-show="userType != '消费者'" type="primary" plain style="margin-left: 220px;" @click="submittracedata()">提 交</el-button>
+        <el-button v-show="userType != '消費者'" type="primary" plain style="margin-left: 220px;" @click="submittracedata()">提 交</el-button>
       </span>
-      <span v-show="userType == '消费者'" slot="footer" style="color: gray;" class="dialog-footer">
-        消费者没有权限录入！请使用溯源功能!
+      <span v-show="userType == '消費者'" slot="footer" style="color: gray;" class="dialog-footer">
+        消費者沒有權限輸入！請使用溯源功能!
       </span>
     </div>
   </div>
@@ -142,7 +142,7 @@ export default {
       console.log(this.tracedata)
       const loading = this.$loading({
         lock: true,
-        text: '数据上链中...',
+        text: '資料上鏈中...',
         spinner: 'el-icon-loading',
         background: 'rgba(0, 0, 0, 0.7)'
       })
@@ -150,21 +150,21 @@ export default {
       formData.append('traceability_code', this.tracedata.traceability_code)
       // 根据不同的用户给arg1、arg2、arg3..赋值,
       switch (this.userType) {
-        case '种植户':
+        case '養殖戶':
           formData.append('arg1', this.tracedata.Farmer_input.Fa_fruitName)
           formData.append('arg2', this.tracedata.Farmer_input.Fa_origin)
           formData.append('arg3', this.tracedata.Farmer_input.Fa_plantTime)
           formData.append('arg4', this.tracedata.Farmer_input.Fa_pickingTime)
           formData.append('arg5', this.tracedata.Farmer_input.Fa_farmerName)
           break
-        case '工厂':
+        case '加工廠':
           formData.append('arg1', this.tracedata.Factory_input.Fac_productName)
           formData.append('arg2', this.tracedata.Factory_input.Fac_productionbatch)
           formData.append('arg3', this.tracedata.Factory_input.Fac_productionTime)
           formData.append('arg4', this.tracedata.Factory_input.Fac_factoryName)
           formData.append('arg5', this.tracedata.Factory_input.Fac_contactNumber)
           break
-        case '运输司机':
+        case '物流公司':
           formData.append('arg1', this.tracedata.Driver_input.Dr_name)
           formData.append('arg2', this.tracedata.Driver_input.Dr_age)
           formData.append('arg3', this.tracedata.Driver_input.Dr_phone)
@@ -183,13 +183,13 @@ export default {
         if (res.code === 200) {
           loading.close()
           this.$message({
-            message: '上链成功，交易ID：' + res.txid + '\n溯源码：' + res.traceability_code,
+            message: '上鏈成功，交易ID：' + res.txid + '\n溯源碼：' + res.traceability_code,
             type: 'success'
           })
         } else {
           loading.close()
           this.$message({
-            message: '上链失败',
+            message: '上鏈失敗',
             type: 'error'
           })
         }
